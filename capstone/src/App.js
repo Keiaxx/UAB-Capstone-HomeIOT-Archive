16ac87a831ed8cf44b8681caa5831fbbe575eac9
chris brown <sevrc1136@gmail.com>
2019-09-07 11:36:33 -0500

    last push for template setup

--- capstone/src/App.js
+++ capstone/src/App.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 function App() {
