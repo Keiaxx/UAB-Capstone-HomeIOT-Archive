39e605a9964781b130805eb556afce9283ef33af
chris brown <sevrc1136@gmail.com>
2019-09-07 10:49:31 -0500

    basic template for front end

---
+++ capstone/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello one and all to this template
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
