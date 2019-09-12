9ec5b95a683e58e6d06efd297a62d7f74cbbbf9c
Chris <sevrc@uab.edu>
2019-09-12 14:11:11 -0500

    added components as templates

---
+++ capstone/src/components/dashboard.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function dashBoard() {
+  
+  return (
+    <div className='LeaderBoard'>
+      <h1>DashBoard </h1>
+
+    </div>
+  );
+}
+
+export default dashBoard;
---
+++ capstone/src/components/stats.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+function stats() {
+  
+  return (
+    <div className='statsBoard'>
+      <h1>statsBoard </h1>
+
+    </div>
+  );
+}
+
+export default stats;
