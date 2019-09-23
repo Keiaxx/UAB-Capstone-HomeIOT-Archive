a3c770c05e2d673e73899d706da20a4d74883f65
Senay H Patel <spatel11@uab.edu>
2019-09-22 22:43:05 -0500

    Updated weather_data.

--- backend/data_generator/weather_data.py
+++ backend/data_generator/weather_data.py
@@ -15,27 +15,49 @@ import certifi
 import ssl
 
 
+"""
+Make an instance of this
+with two atributes city_name
+and state_name.
+place = Location(city_name,state_name)
+"""
 class Location():
     def __init__(self, city_name:  str, state_name: str):
         self.city_name =city_name
         self.state_name = state_name
+
         
+    """
+    CAll this function to get
+    latitude of a place
+    """
     def latitude(self) -> float:
         place = self.city_name + " " + self.state_name
         geolocator = Nominatim(user_agent="darksky1")
         locate = geolocator.geocode(place)     
         return locate.latitude
+
     
+    """
+    CAll this function to get
+    longitude of the place
+    """
     def longitude(self) -> float:
         place = self.city_name + " " + self.state_name
         geolocator = Nominatim(user_agent="darksky1")
         locate = geolocator.geocode(place)     
         return locate.longitude
 
+    
+    """
+    The data of a date will be generated hourly
+    in dictionary where hours(1-24) are used as
+    keys and temperature is value.
+    """
     def one_day_temp(self, date):
         lat=  self.latitude()
         long = self.longitude()
-        key = "714247b8f24a0afa9474e19df782ceeb"
+        key = "Add the API key of darksky here"
         PLACE = key, lat, long
         yy = date.year
         mm = date.month
@@ -50,11 +72,16 @@ class Location():
             data[b] = a[x]
             b += 1
         return data
+
     
+    """
+    The data will be generated of a
+    particular hour of a particular date.
+    """
     def one_hour_temp(self, date, hour) -> float:
         lat =  self.latitude()
         long = self.longitude()
-        key = "714247b8f24a0afa9474e19df782ceeb"
+        key = "Add the API key of darksky here"
         PLACE = key, lat, long
         yy = date.year
         mm = date.month
@@ -63,7 +90,12 @@ class Location():
         t = dt(yy,mm, dd, hh).isoformat()
         place = forecast(*PLACE, time=t)
         return place.hourly[hour].temperature
+
     
+    """
+    This methode is used to
+    change the location.
+    """
     def change_city(self,city_name, state_name):
         self.city_name =city_name2w
         self.state_name =state_name
@@ -71,27 +103,3 @@ class Location():
     
 
     
-"""Test CASES are given below
-   just to give an idea how
-   the code goes """
-
-
-#start_date =dt(2019, 9, 17)
-#end_date = dt(2019, 9, 17)
-
-##start_date =dt(2019, 9, 22)
-##end_date = dt(2019, 9, 22)
-##
-####temperatures("birmingham", "alabama", start_date, end_date)
-##
-###print(data,date(2019, 9, 18))
-##place= Location("Chelsea", "Alabama")
-##print(place.latitude(), place.longitude(), place.one_day_temp(start_date))
-
-
-##place.change_city("Birmingham", "Alabama")
-##print(place.latitude(), place.longitude(), place.one_day_temp(start_date))
-##tempreature = place.one_day_temp(start_date)
-##print(tempreature[1])
-##tempreature = place.one_hour_temp(dt(2019, 9, 22), 13)
-##print(tempreature)
