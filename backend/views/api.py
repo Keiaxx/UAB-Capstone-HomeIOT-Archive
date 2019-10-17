6c4dcf8c5e677b21fa4c7d22fa906d44716735ea
Adrian Gose <adrian@gose.pw>
2019-10-17 23:19:08 +0000

    Fix swagger.json endpoint behind nginx

--- backend/views/api.py
+++ backend/views/api.py
@@ -1,7 +1,4 @@
-# Contributor(s): Adrian Gose
-# If you worked on this file, add your name above so we can keep track of contributions
-
-from flask import Blueprint
+from flask import Blueprint, url_for, request
 from flask_restplus import Api
 
 # Define API blueprints and API spec for flask
@@ -13,3 +10,15 @@ api = Api(apiv1, version='1.0', title='HomeIOT API',
 locations_ns = api.namespace('location', 'Location methods')
 devices_ns = api.namespace('device', 'Device methods')
 usage_ns = api.namespace('usage', 'Device usage information')
+
+# Fix of returning swagger.json on HTTP
+@property
+def specs_url(self):
+    """
+    The Swagger specifications absolute url (ie. `swagger.json`)
+
+    :rtype: str
+    """
+    return url_for(self.endpoint('specs'), _external=False)
+
+Api.specs_url = specs_url
