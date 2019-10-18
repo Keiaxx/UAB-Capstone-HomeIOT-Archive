a36ad14eaf7afee5f908fc09fd1f5009143cd446
Adrian Gose <adrian@gose.pw>
2019-10-18 17:37:19 -0500

    Add demo switchboard with connection to live API with Axios

--- capstone/src/components/switchboard.js
+++ capstone/src/components/switchboard.js
@@ -1,45 +1,116 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import Button from '@material-ui/core/Button';
+import LinearProgress from "@material-ui/core/LinearProgress";
+import Switch from "@material-ui/core/Switch";
+import Button from "@material-ui/core/Button";
+
+import { fetchDevices, setDeviceState } from "../actions";
+
+function DeviceItem(props) {
+
+    let [state, setState] = React.useState({
+      checked: props.device.state,
+    });
+
+    let handleDeviceStateChange = (dispatch, device) => {
+        return event => {
+            console.log(
+                "Device state mutated for ID: " +
+                    device.deviceId +
+                    " setting to " +
+                    event.target.checked
+            );
+            let checked = event.target.checked;
+            device.state = checked;
+            setState({ ...state, checked: checked });
+            dispatch(setDeviceState(device.deviceId, checked));
+        };
+    }
+
+
+    return (
+      <li>
+          Device: {props.device.name} |
+          <Switch
+              checked={state.checked}
+              onChange={handleDeviceStateChange(
+                  props.dispatch,
+                  props.device
+              )}
+          />
+          {props.device.state ? "ON" : "OFF"}
+      </li>
+  );
+}
+
+function DeviceList(props) {
+    const dispatch = props.dispatch;
+    const rawList = props.devicelist;
+
+    console.log(props.devices);
+
+    const deviceList = rawList.map(device => {
+        return (
+            <DeviceItem
+                dispatch={dispatch}
+                device={device}
+                key={device.deviceId}
+            />
+        );
+    });
+
+    return <ul>{deviceList}</ul>;
+}
 
 class switchBoard extends Component {
-    
-      render(){
-        return (
-            <div className='swichboard'>
-                <div className ='kitchen'>
-                    <p>
-                        Kitchen:<br/>
-                            Oven: 
-                            <Button  color="primary" onClick={this.props.TURNED_ON_OVEN}>ON</Button>
-                            <Button  color="primary" onClick={this.props.TURNED_OFF_OVEN}>OFF</Button>
-                            
-                    </p>
-                </div>
-              
-        
-            </div>
-          );
-      }
+    componentDidMount() {
+        console.log("Component mounted, loading devices");
 
+        const { dispatch } = this.props;
+        dispatch(fetchDevices());
+    }
+
+    render() {
+        let loadingBar;
+
+        if (this.props.devices.fetching) {
+            loadingBar = <LinearProgress />;
+        }
+
+        return (
+            <div className="swichboard">
+                {loadingBar}
+                <DeviceList
+                    dispatch={this.props.dispatch}
+                    devicelist={this.props.devicelist}
+                />
+            </div>
+        );
+    }
 }
 
 //for every page you need a mapStateToProps for every component
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
     return {
         age: state.age,
         oven: state.oven,
-        frontDoor: state.frontDoor
-    }
-  }
+        frontDoor: state.frontDoor,
+        devices: state.devices,
+        devicelist: state.devices.list
+    };
+};
 
 //
-const mapDispatchToProps = (dispatch) => {
-return {
-    TURNED_ON_OVEN: () => dispatch({type:'OVEN_ON'}),
-    TURNED_OFF_OVEN: () => dispatch({type:'OVEN_OFF'})
-}
-}
+const mapDispatchToProps = dispatch => {
+    return {
+        TURNED_ON_OVEN: () => dispatch({ type: "OVEN_ON" }),
+        TURNED_OFF_OVEN: () => dispatch({ type: "OVEN_OFF" }),
+        dispatch: dispatch
+    };
+};
 
-  export default connect(mapStateToProps, mapDispatchToProps)(switchBoard);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(switchBoard);
--- capstone/src/index.js
+++ capstone/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
 
 //
 import reducer from './reducers/switch';
@@ -13,9 +14,27 @@ import { ThemeProvider } from '@material-ui/styles';
 import theme from './theme';
 
 /*the following is react-redux*/
+
+const initialState = {
+  age:21,
+  garage1:            ["off", 0, 0],
+  garage2:            ["off", 0, 0],
+  frontDoor:          ["off", 0, 0],
+  backDoor:           ["off", 0, 0],
+  oven:               ["off", 200, 500],
+  washingMachine:     ["off", 0, 0],
+  devices: {
+      fetching: false,
+      list: []
+  },
+};
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  initialState,
+  applyMiddleware(
+    thunk
+  )
   );
 
 //Provider's prop store takes one param, store = {**your allReducers** which is saved in this app as store}
