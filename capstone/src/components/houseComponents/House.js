40cfb0a7ba88ec67bf94fa21096487a9dd9e9e3f
Adrian Gose <adrian@gose.pw>
2019-11-21 20:55:23 +0000

    Add demo for loop location coords

--- capstone/src/components/houseComponents/House.js
+++ capstone/src/components/houseComponents/House.js
@@ -6,6 +6,64 @@ import { makeStyles } from '@material-ui/core/styles';
 import overall from "./overallView.png";
 import LightBulb from "./lightbulb.jpg";
 
+const devicesloc = [
+  {
+    name: 'garage',
+    x: 568,
+    y: 80
+  },
+  {
+    name: 'waterheater',
+    x: 700,
+    y: 180
+  },
+  {
+    name: 'washerdryer',
+    x: 840,
+    y: 140
+  },
+  {
+    name: 'backdoor',
+    x: 870,
+    y: 20
+  },
+  {
+    name: 'oven',
+    x: 935,
+    y: 335
+  },
+  {
+    name: 'fridge',
+    x: 920,
+    y: 415
+  },
+  {
+    name: 'frontdoor',
+    x: 15,
+    y: 710
+  },
+  {
+    name: 'bed1',
+    x: 100,
+    y: 370
+  },
+  {
+    name: 'bed2',
+    x: 330,
+    y: 370
+  },
+  {
+    name: 'bed3',
+    x: 700,
+    y: 370
+  },
+  {
+    name: 'bath',
+    x: 540,
+    y: 370
+  },
+]
+
 const useStyles = makeStyles(theme => ({
   housediv: {
     float: 'left',
@@ -23,6 +81,20 @@ const useStyles = makeStyles(theme => ({
 
 function House() {
   let classes = useStyles();
+  let items = []
+
+  for (let it in devicesloc) {
+    let device = devicesloc[it]
+
+    items.push(<img src={LightBulb} style={
+      {
+        top: device.y,
+        left: device.x,
+        width: '50px',
+        position: 'absolute'
+      }
+    } />)
+  }
 
   return (
     <div className={classes.housediv}>
@@ -35,14 +107,9 @@ function House() {
           backgroundSize: '200px 200px'
         }} />
 
-      <img src={LightBulb} style={
-        {
-          top: 370,
-          left: 700,
-          width: '50px',
-          position: 'absolute'
-        }
-      } />
+
+
+      {items}
     </div>
   );
 }
@@ -66,6 +133,10 @@ const mapStateToProps = (state) => {
 
 
 export default connect(mapStateToProps)(House);
+
+
+
+
 
 /*
 garage = 568 , 80
