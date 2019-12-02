bf8abdfd2a9f4ef42fcc40dcb7b4e07ff04cdc39
Adrian Gose <adrian@gose.pw>
2019-12-02 19:00:42 +0000

    Update thermostat stuff and state names

--- capstone/src/components/houseComponents/House.js
+++ capstone/src/components/houseComponents/House.js
@@ -3,6 +3,10 @@ import { connect } from 'react-redux';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
 //imported house components
 import overall from "./overallView.png";
 import LightBulb from "./lightbulb.jpg";
@@ -49,9 +53,6 @@ class House extends Component {
           position: 'relative'
         }
       }>
-          <Button variant="contained" color="secondary"> +   </Button><br/>
-          <Button variant="contained"> SET </Button> <br/>
-          <Button variant="contained" color="primary"> - </Button>
         <img
           style={
             {
@@ -68,8 +69,6 @@ class House extends Component {
 
 
         <DeviceList devices={this.props.devicelist} />
-
-        {JSON.stringify(this.props.hvac.set_f)}
       </div>
     );
   }
--- capstone/src/components/houseComponents/TodaysWeather.js
+++ capstone/src/components/houseComponents/TodaysWeather.js
@@ -5,6 +5,10 @@ import Typography from '@material-ui/core/Typography';
 
 import { fetchDevices, getHVAC } from "../../actions";
 
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container';
+import { Button } from '@material-ui/core';
+
 
 class Temp extends Component {
   constructor(props) {
@@ -14,12 +18,34 @@ class Temp extends Component {
     dispatch(getHVAC());
   }
 
-  render(){
+  render() {
     return (
       <React.Fragment>
-        <Typography component="p" variant="h6">
-        {JSON.stringify(this.props.hvac.set_f)}      
-        </Typography>
+        <Grid container spacing={5}>
+          <Grid item xs={4}>
+            Int:
+            <Typography component="p" variant="h2">
+              {JSON.stringify(this.props.hvac.set_f)}
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            Ext:
+            <Typography component="p" variant="h2">
+              {JSON.stringify(this.props.hvac.set_f)}
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            Set:
+            <Typography component="p" variant="h2">
+              {JSON.stringify(this.props.hvac.set_f)}
+            </Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Button variant="contained" color="secondary"> + </Button><br />
+            <Button variant="contained"> SET </Button> <br />
+            <Button variant="contained" color="primary"> - </Button>
+          </Grid>
+        </Grid>
       </React.Fragment>
     );
   }
